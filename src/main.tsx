248cc8f2562417d7b28ceb55ecaff7719f1cc246
yutao <tao.yu@thoughtworks.com>
2023-07-14 11:49:47 +0800

    feat: support antd App global configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {App as AppProvider} from 'antd'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <AppProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AppProvider>,
 )
